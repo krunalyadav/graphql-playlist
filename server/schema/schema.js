a307f448724d031fe48b414e0b2f45959bf1d955
Krunal Yadav <krunal.yadav18@gmail.com>
2019-02-03 00:40:37 +0530

    Add book and author schema

---
+++ server/schema/schema.js
@@ -0,0 +1,68 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: {
+      type: GraphQLString
+    },
+    genre: {
+      type: GraphQLString
+    }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {}
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {}
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
